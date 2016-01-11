80a19224f3b842653db770cd81a93af73a0a5f16
rstam <robert@robertstam.org>
2016-01-11 09:58:19 -0500

    Initial commit.

---
+++ src/FSharpBsonDocument/BsonType.fs
@@ -0,0 +1,22 @@
+﻿namespace FSharpBsonDocument
+
+type BsonType =
+    | Double = 1
+    | String = 2
+    | Document = 3
+    | Array = 4
+    | Binary = 5
+    | Undefined = 6
+    | ObjectId = 7
+    | Boolean = 8
+    | DateTime = 9
+    | Null = 10
+    | RegularExpression = 11
+    | JavaScript = 13
+    | JavaScriptWithScope = 15
+    | Int32 = 16
+    | Timestamp = 17
+    | Int64 = 18
+    | MaxKey = 127
+    | MinKey = 255
+
