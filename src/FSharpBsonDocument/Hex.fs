9612c0a501a03e6371464d3299ec6b3cb7b99552
rstam <robert@robertstam.org>
2016-01-21 17:30:50 -0500

    Added tests for Hex.

--- src/FSharpBsonDocument/Hex.fs
+++ src/FSharpBsonDocument/Hex.fs
@@ -4,6 +4,7 @@ open System
 open System.Globalization
 
 let ToByteArray (hex : string) =
+    let hex = if hex.Length % 2 = 0 then hex else "0" + hex
     [0 .. 2 .. (hex.Length - 1)]
     |> Seq.map (fun i -> hex.Substring(i, 2))
     |> Seq.map (fun x -> Byte.Parse(x, NumberStyles.AllowHexSpecifier))
---
+++ tests/FSharpBsonDocumentTests/HexTests.fs
@@ -0,0 +1,39 @@
+﻿namespace FSharpBsonDocumentTests
+
+open FSharpBsonDocument
+open Xunit
+
+type HexTests =
+    class
+        new() =
+            {}
+
+        [<Theory>]
+        [<InlineData("", 0, 0)>]
+        [<InlineData("0", 1, 0)>]
+        [<InlineData("00", 1, 0)>]
+        [<InlineData("001", 2, 0)>]
+        [<InlineData("0001", 2, 0)>]
+        [<InlineData("000102", 3, 0)>]
+        [<InlineData("000102", 3, 0)>]
+        [<InlineData("f0", 1, 0xf0)>]
+        [<InlineData("f0f1", 2, 0xf0)>]
+        [<InlineData("f0f1f2", 3, 0xf0)>]
+        member this.``ToByteArray should return expected result``(value : string, length : int, offset : int) =
+            let bytes = Seq.init length (fun i -> byte (offset + i)) |> Seq.toArray
+            let result = Hex.ToByteArray(value)
+            Assert.Equal<byte>(bytes, result)
+
+        [<Theory>]
+        [<InlineData(0, 0, "")>]
+        [<InlineData(1, 0, "00")>]
+        [<InlineData(2, 0, "0001")>]
+        [<InlineData(3, 0, "000102")>]
+        [<InlineData(1, 0xf0, "f0")>]
+        [<InlineData(2, 0xf0, "f0f1")>]
+        [<InlineData(3, 0xf0, "f0f1f2")>]
+        member this.``ToHex should return expected result``(length : int, offset : int, expectedResult : string) =
+            let bytes = Seq.init length (fun i -> byte (offset + i)) |> Seq.toArray
+            let result = Hex.ToHex(bytes)
+            Assert.Equal(expectedResult, result)
+    end
